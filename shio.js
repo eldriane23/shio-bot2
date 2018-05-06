98373e9526daa0af87dc5008cae0c27478018d4d
Eldriane <iotakuinfo@gmail.com>
2018-05-05 18:32:48 -1000

    make it better

--- shio.js
+++ shio.js
@@ -15,7 +15,7 @@ client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
-  client.user.setPresence({game: {name: "+help or +helpdm", type: 3}});
+  client.user.setPresence({game: {name: "+help or +helpdm", type: 2}});
 });
 
 
